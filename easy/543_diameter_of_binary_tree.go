ee7c5c7b280972f1933f8cce97613c21d8fdd0c5
rzwei <rzwei@tuta.io>
2024-10-14 23:37:44 +0000

    chore: refactor 543 and add comments

--- easy/543_diameter_of_binary_tree.go
+++ easy/543_diameter_of_binary_tree.go
@@ -15,23 +15,34 @@ import (
 
 // @lc code=start
 func diameterOfBinaryTree(root *TreeNode) int {
-	maxDiameter := 0
-	maxDepth(root, &maxDiameter)
+	// track the maximum diameter
+	_, maxDiameter := maxDepth(root)
 
 	return maxDiameter
 }
 
-func maxDepth(root *TreeNode, maxDiameter *int) int {
+// return depth and diameter
+func maxDepth(root *TreeNode) (int, int) {
+	// base case
+	// 0 depth for nil node
 	if root == nil {
-		return 0
+		return 0, 0
 	}
 
-	leftDepth := maxDepth(root.Left, maxDiameter)
-	rightDepth := maxDepth(root.Right, maxDiameter)
+	// recursive case
+	// post order traversal
+	// calculating the depth of left nodes first
+	// then right nodes
+	leftDepth, leftDiameter := maxDepth(root.Left)
+	rightDepth, rightDiameter := maxDepth(root.Right)
 
-	*maxDiameter = max(*maxDiameter, leftDepth+rightDepth)
+	// process the root node (parent node)
+	// calculate the max diameter
+	// piggyback max depth calculation
+	maxDiameter := max(leftDiameter, rightDiameter, leftDepth+rightDepth)
 
-	return 1 + max(leftDepth, rightDepth)
+	// return the depth of node
+	return 1 + max(leftDepth, rightDepth), maxDiameter
 }
 
 // @lc code=end
