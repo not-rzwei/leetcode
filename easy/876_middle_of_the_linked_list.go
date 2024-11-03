74b24831a9e3c4bb6b68806853b9ce4fde9ffb1a
rzwei <rzwei@tuta.io>
2024-11-03 08:32:29 +0000

    chore: solve 876 middle of linked list

---
+++ easy/876_middle_of_the_linked_list.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=876 lang=golang
+ *
+ * [876] Middle of the Linked List
+ */
+
+package easy
+
+import . "leetcode/helper"
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+// @lc code=end
